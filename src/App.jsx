a4e80ef8f697b5e838f7755c6ad9332cf67d3f7d
Mohamed M.Khalaf <mohamedmohamedkhalaf47@gmail.com>
2025-05-04 00:14:37 +0300

    Completed 3 Pages & Added Registration Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { Route, Routes, useLocation } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Room from "./pages/Room";
+import MyBookings from "./pages/MyBookings";
+import HotelRegistration from "./components/HotelRegistration";
 
 function App() {
-  const isOwnerPath = useLocation().pathname.includes("owner");
+	const isOwnerPath = useLocation().pathname.includes("owner");
 	return (
 		<div>
 			{!isOwnerPath && <Navbar />}
+			{false && <HotelRegistration />}
 			<div className="min-h-[70vh]">
 				<Routes>
-					<Route path="/" element={<Home />}/>
+					<Route path="/" element={<Home />} />
+					<Route path="/rooms" element={<Rooms />} />
+					<Route path="/rooms/room/:id" element={<Room />} />
+					<Route path="/my-bookings" element={<MyBookings />} />
 				</Routes>
 			</div>
 			<Footer />
