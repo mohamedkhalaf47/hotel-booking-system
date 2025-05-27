4b6aa4845ba65a0c048f68d65f009bd2437a2267
Mohamed M.Khalaf <mohamedmohamedkhalaf47@gmail.com>
2025-05-27 19:04:51 +0300

    Managed Links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,9 +73,9 @@ const Navbar = () => {
 			{/* Desktop Nav */}
 			<div className="hidden md:flex items-center gap-4 lg:gap-8">
 				{navLinks.map((link, i) => (
-					<a
+					<Link
 						key={i}
-						href={link.path}
+						to={link.path}
 						className={`group flex flex-col gap-0.5 ${
 							isScrolled ? "text-gray-700" : "text-white"
 						}`}
@@ -86,7 +86,7 @@ const Navbar = () => {
 								isScrolled ? "bg-gray-700" : "bg-white"
 							} h-0.5 w-0 group-hover:w-full transition-all duration-300`}
 						/>
-					</a>
+					</Link>
 				))}
 				{user && (
 					<button
